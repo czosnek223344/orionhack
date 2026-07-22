4030ef4b766186152f665e7f2e7d5677e044ffdf
4Fix <maybeczosnek223344@gmail.com>
2026-07-23 01:31:13 +0200

    fuck ai

--- orionhack/src/main/java/com/orionhack/addon/utils/MaceKillUtil.java
+++ orionhack/src/main/java/com/orionhack/addon/utils/MaceKillUtil.java
@@ -1,38 +1,44 @@
 package com.orionhack.addon.utils;
 
-import com.orionhack.addon.modules.Settings;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
+import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.util.Hand;
-import static meteordevelopment.meteorclient.MeteorClient.mc;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import com.orionhack.addon.modules.Settings;
+import com.orionhack.addon.utils.TpUtil;
 
 public class MaceKillUtil {
+    private static final MinecraftClient mc = MinecraftClient.getInstance();
 
-    public static void executeMaceKill(Entity target) {
-        if (!Settings.maceKillEnabled) {
-            return;
+    private static double MACE_KILL_HEIGHT = 30.0;
+
+    public static void hit(Entity target) {
+        if (mc.player == null || target == null) return;
+
+        double x = mc.player.getX();
+        double y = mc.player.getY();
+        double z = mc.player.getZ();
+
+        double finalHeight = MACE_KILL_HEIGHT;
+        Settings settingsModule = Modules.get().get(Settings.class);
+        
+        if (settingsModule != null && settingsModule.isActive()) {
+            finalHeight = settingsModule.maceKillHeight.get();
         }
 
-        if (mc.player == null || target == null) {
-            return;
-        }
+        TpUtil.teleport(x, y + finalHeight, z);
+        
+        TpUtil.teleport(x, y, z);
 
-        double pX = mc.player.getX();
-        double pY = mc.player.getY();
-        double pZ = mc.player.getZ();
+        mc.player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(target, mc.player.isSneaking()));
 
-        double tX = target.getX();
-        double tY = target.getY();
-        double tZ = target.getZ();
-
-        double tpHeight = Settings.maceHeightAmount;
-
-        TpUtil.teleport(pX, pY + tpHeight, pZ);
-
-        mc.player.fallDistance = (float) tpHeight;
-
-        TpUtil.teleport(tX, tY, tZ);
-
-        mc.interactionManager.attackEntity(mc.player, target);
         mc.player.swingHand(Hand.MAIN_HAND);
+        mc.player.networkHandler.sendPacket(new HandSwingC2SPacket(Hand.MAIN_HAND));
+    }
+
+    public static void setMaceKillHeight(double height) {
+        MACE_KILL_HEIGHT = height;
     }
 }

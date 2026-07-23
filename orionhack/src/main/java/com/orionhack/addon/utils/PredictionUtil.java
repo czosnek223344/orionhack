0f4784a01a402a9af3f66849be3c26dcde7924a5
4Fix <maybeczosnek223344@gmail.com>
2026-07-23 02:06:31 +0200

    Update PredictionUtil.java

--- orionhack/src/main/java/com/orionhack/addon/utils/PredictionUtil.java
+++ orionhack/src/main/java/com/orionhack/addon/utils/PredictionUtil.java
@@ -1,16 +1,63 @@
 package com.orionhack.addon.utils;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 
 public class PredictionUtil {
+
     private static final MinecraftClient mc = MinecraftClient.getInstance();
 
-    public static Vec3d getPredictedPos(PlayerEntity target, double ticks) {
-        if (target == null) return null;
-        Vec3d velocity = target.getVelocity();
-        Vec3d currentPos = new Vec3d(target.getX(), target.getY(), target.getZ());
-        return currentPos.add(velocity.multiply(ticks));
+    public static Vec3d getPredictedPosition(Entity entity) {
+        if (entity == null || mc.world == null) {
+            return entity != null ? new Vec3d(entity.getX(), entity.getY(), entity.getZ()) : Vec3d.ZERO;
+        }
+
+        if (!(entity instanceof PlayerEntity)) {
+            return new Vec3d(entity.getX(), entity.getY(), entity.getZ());
+        }
+
+        Vec3d lastPos = new Vec3d(entity.prevX, entity.prevY, entity.prevZ);
+        Vec3d currentPos = new Vec3d(entity.getX(), entity.getY(), entity.getZ());
+        Vec3d velocity = currentPos.subtract(lastPos);
+
+        if (velocity.lengthSquared() < 0.0001) {
+            return currentPos;
+        }
+
+        double totalSpeed = Math.sqrt(velocity.lengthSquared());
+
+        double ticksAhead = Math.max(1.0, Math.min(10.0, totalSpeed * 0.4));
+        Vec3d predicted = currentPos.add(velocity.multiply(ticksAhead));
+
+        double yBonus = totalSpeed >= 2.0 ? Math.min(2.5, totalSpeed * 0.08) : 0.0;
+        Vec3d targetPos = new Vec3d(predicted.x, predicted.y + yBonus, predicted.z);
+
+        RaycastContext context = new RaycastContext(
+            currentPos,
+            targetPos,
+            RaycastContext.ShapeType.COLLIDER,
+            RaycastContext.FluidHandling.NONE,
+            entity
+        );
+
+        BlockHitResult hit = mc.world.raycast(context);
+
+        if (hit.getType() == HitResult.Type.BLOCK) {
+            Vec3d hitPos = hit.getPos();
+            Vec3d pullBackDirection = currentPos.subtract(hitPos);
+
+            if (pullBackDirection.lengthSquared() > 0.0001) {
+                return hitPos.add(pullBackDirection.normalize().multiply(0.35));
+            } else {
+                return currentPos;
+            }
+        }
+
+        return targetPos;
     }
 }

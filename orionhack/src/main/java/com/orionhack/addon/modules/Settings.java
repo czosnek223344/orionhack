b17a586d4782582683a6673214a71451ec93c6ce
4Fix <maybeczosnek223344@gmail.com>
2026-07-23 12:56:37 +0200

    Update Settings.java

--- orionhack/src/main/java/com/orionhack/addon/modules/Settings.java
+++ orionhack/src/main/java/com/orionhack/addon/modules/Settings.java
@@ -1,7 +1,6 @@
 package com.orionhack.addon.modules;
 
 import com.orionhack.addon.OrionHack;
-import com.orionhack.addon.utils.MaceKillUtil;
 import com.orionhack.addon.utils.TPUtil;
 import com.orionhack.addon.utils.TargetUtil;
 import meteordevelopment.meteorclient.events.world.TickEvent;
@@ -16,12 +15,12 @@ public class Settings extends Module {
 
     public final Setting<Double> maceKillHeight = sgGeneral.add(new DoubleSetting.Builder()
         .name("mace-kill-height")
-        .description("height for mace kill.")
-        .defaultValue(30.0)
+        .description("height for mace kill attack.")
+        .defaultValue(20.0)
         .min(0.0)
-        .max(50.0)
+        .max(30.0)
         .sliderMin(0.0)
-        .sliderMax(50.0)
+        .sliderMax(30.0)
         .build()
     );
 
@@ -31,11 +30,9 @@ public class Settings extends Module {
 
     @Override
     public void onActivate() {
-        MaceKillUtil.setMaceKillHeight(maceKillHeight.get());
     }
 
     @EventHandler
     private void onTick(TickEvent.Post event) {
-        MaceKillUtil.setMaceKillHeight(maceKillHeight.get());
     }
-} // you have found secret backdoor, be scared
+}
